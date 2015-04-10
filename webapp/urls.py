08057499f84794ee932fea74504048ac93088d85
Andrew Kelleher <akellehe@gmail.com>
2015-04-10 11:01:03 -0400

    Added view, endpoint for /wsgi

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from webapp.views import wsgi
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'wsgi_talk.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^wsgi/', wsgi)
 ]
